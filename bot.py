09a006091c01da7ce36fbb5c97a2e3abc252841a
xPolar <harismehrzad12@gmail.com>
2021-03-03 13:28:23 -0800

    Redo bot.py file. Use Jishaku for evaluating rudimentry Python code. Jishaku is able to reload extensions so no need for the reload command anymore. Sharding, join / leave logs, typings, and support for intents added.

--- bot.py
+++ bot.py
@@ -1,35 +1,148 @@
-import config, discord
+# Packages.
+## Packages default to Python.
+import datetime
+from asyncio import sleep
+## Packages that have to be installed through the package manager.
+import aiohttp, discord
+from colorama import Fore, Style, init
 from discord.ext import commands
-import sentry_sdk
-sentry_sdk.init("https://7cf2a61985d449e6ab27c39cf32bb8c8@sentry.io/1495910")
+## Packages on this machine.
+import config
 
-bot = commands.Bot(command_prefix = config.DEFAULT_PREFIX, case_insensitive = True)
+# Initialize colorama
+init()
 
-initial_extensions = ['cogs.utils',
-                      'cogs.main_commands',
-                      'cogs.inventory']
+def get_prefix(bot : commands.AutoShardedBot, message : discord.Message) -> list:
+    """Returns the bot's prefix.
 
-for cog in initial_extensions:
-    print("loading cog %s..." % cog)
-    bot.load_extension(cog)
-print("cogs loaded")
+    Args:
+        bot (commands.AutoShardedBot): The bot object.
+        message (discord.Message): Message object.
 
-@bot.command(name="reload")
-async def reload(ctx, module:str=None):
-    if ctx.author.id not in [229695200082132993, 245653078794174465, 282565295351136256]:
+    Returns:
+        list: The prefixes the bot will accept.
+    """
+
+    return commands.when_mentioned_or(">")(bot, message)
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.AutoShardedBot(activity = discord.Game(f"with {config.prefix}help"), command_prefix = get_prefix, case_insensitive = True, intents = intents, status = discord.Status.dnd)
+
+bot.load_extension("jishaku")
+
+# Loads all of our cogs.
+for cog in config.cogs:
+    bot.load_extension(f"cogs.{cog}")
+    print(f"{Style.BRIGHT}{Fore.GREEN}[SUCCESS]{Fore.WHITE} Loaded Cog: {cog}")
+
+async def owner(ctx) -> bool:
+    """Checks if a user is allowed to run the restart.
+
+    Args:
+        ctx (discord.py's context object): Context object.
+
+    Returns:
+        bool: Wether the user is one of the bot's owners.
+    """
+
+    return ctx.author.id in config.ownerids
+
+@bot.event
+async def on_command_error(ctx : commands.Context, error : Exception) -> None:
+    """Bot wide error handler.
+
+    Args:
+        ctx (commands.Context): Discord's context object.
+        error (Exception): The error that was raised.
+    """
+
+    if isinstance(error, (commands.CommandNotFound, commands.CheckFailure, commands.BadUnionArgument, commands.BotMissingPermissions)):
         return
+    else:
+        try:
+            embed = discord.Embed(
+                title = "Error",
+                description = f"**```\n{error}\n```**".replace(config.token, "[ R E D A C T E D ]"),
+                color = config.errorcolor
+            )
+            embed.set_footer(text = "Please report this to Polar#6880")
+            await ctx.send(embed = embed)
+        finally:
+            raise error
 
-    if module in initial_extensions and module != None:
-        await ctx.send("reloading cog %s..." % module)
-        bot.reload_extension(module)
-        return
+@bot.event
+async def on_guild_join(guild : discord.Guild) -> None:
+    """When the bot joins a server send a webhook with detailed information as well as print out some basic information.
 
-    await ctx.send("reloading commands..")
-    for cog in initial_extensions:
-        print("reloading cog %s..." % cog)
-        await ctx.send("reloading cog %s..." % cog)
-        bot.reload_extension(cog)
 
-    await ctx.send(content="commands reloaded.")
+    Args:
+        guild (discord.Guild): The guild we joined.discord
+    """
 
-bot.run(config.TOKEN)
+    embed = discord.Embed(
+        title = "Joined a server!",
+        timestamp = datetime.datetime.utcnow(),
+        color = 0x77DD77
+    )
+    embed.add_field(name = "Server Name", value = guild.name)
+    embed.add_field(name = "Server Members", value = len(guild.members) - 1)
+    embed.add_field(name = "Server ID", value = guild.id)
+    embed.add_field(name = "Server Owner ID", value = guild.owner.id)
+    embed.set_footer(text = f"I am now in {len(bot.guilds)} servers", icon_url = guild.icon_url)
+    owner = bot.get_user(guild.owner_id)
+    if owner:
+        embed.add_field(name = "Server Owner", value = f"{owner}")
+    async with aiohttp.ClientSession() as session:
+        webhook = discord.Webhook.from_url(config.webhook, adapter = discord.AsyncWebhookAdapter(session))
+        await webhook.send(embed = embed, username = "Joined a server")
+    print(f"{Style.BRIGHT}{Fore.LIGHTGREEN_EX}[JOINED-SERVER]{Fore.WHITE} Joined {Fore.YELLOW}{guild.name}{Fore.WHITE} with {Fore.YELLOW}{len(guild.members) - 1}{Fore.WHITE} members.")
+
+@bot.event
+async def on_guild_remove(guild : discord.Guild) -> None:
+    """When the bot leaves a server send a webhook with detailed information as well as print out some basic information.
+
+
+    Args:
+        guild (discord.Guild): The guild we joined.discord
+    """
+
+    embed = discord.Embed(
+        title = "Left a server!",
+        timestamp = datetime.datetime.utcnow(),
+        color = 0xFF6961
+    )
+    embed.add_field(name = "Server Name", value = guild.name)
+    embed.add_field(name = "Server Members", value = len(guild.members))
+    embed.add_field(name = "Server ID", value = guild.id)
+    embed.add_field(name = "Server Owner ID", value = guild.owner.id)
+    embed.set_footer(text = f"I am now in {len(bot.guilds)} servers", icon_url = guild.icon_url)
+    owner = bot.get_user(guild.owner_id)
+    if owner:
+        embed.add_field(name = "Server Owner", value = f"{owner}")
+    async with aiohttp.ClientSession() as session:
+        webhook = discord.Webhook.from_url(config.webhook, adapter = discord.AsyncWebhookAdapter(session))
+        await webhook.send(embed = embed, username = "Left a server")
+    print(f"{Style.BRIGHT}{Fore.LIGHTRED_EX}[LEFT-SERVER]{Fore.WHITE} Left {Fore.YELLOW}{guild.name}{Fore.WHITE} with {Fore.YELLOW}{len(guild.members)}{Fore.WHITE} members.")
+
+@bot.event
+async def on_shard_ready(shard_id : int) -> None:
+    """When a shard starts print out that the shard has started.
+
+    Args:
+        shard_id (int): The ID of the shard that has started. (Starts from 0).
+    """
+
+    print(f"{Style.BRIGHT}{Fore.CYAN}[SHARD-STARTED]{Fore.WHITE} Shard {Fore.YELLOW}{shard_id}{Fore.WHITE} has started!")
+
+@bot.event
+async def on_ready() -> None:
+    """When the bot fully starts print out that the bot has started and set the status."""
+
+    print(f"{Style.BRIGHT}{Fore.CYAN}[BOT-STARTED]{Fore.WHITE} I'm currently in {len(bot.guilds)} servers with {len(bot.users)} users!")
+    while True:
+        await bot.change_presence(status = discord.Status.dnd, activity = discord.Game(f"with {config.prefix}help"))
+        await sleep(1800)
+
+# Start the bot.
+bot.run(config.token)
